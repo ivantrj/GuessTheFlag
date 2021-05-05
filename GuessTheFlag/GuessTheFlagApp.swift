c1c65ce43126c284553a403550d86859eb7e5a25
ivantrj <ivantrajanovski@gmail.com>
2021-05-05 09:57:49 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ivan Trajanovski on 2021. 05. 05..
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
