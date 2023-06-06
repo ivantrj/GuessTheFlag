59298edd7d8c9f68bb4c02269dad8ddff3a630ac
Ivan Trj <ivantrajanovski@gmail.com>
2023-06-06 10:50:45 +0200

    fix flag tapping

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
                 
                 ForEach(0..<3) { number in
                     Button {
-                        
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
